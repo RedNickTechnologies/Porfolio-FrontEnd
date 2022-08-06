30b0c1cdb5ab5d9f372d1e65c1049d2ad052479b
Nicolas <ngacevedo5@gmail.com>
2022-08-06 18:21:15 -0300

    corrigiendo y creando Banner
    la imagen es temporal

--- Porfolio-FE/src/app/app.module.ts
+++ Porfolio-FE/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
