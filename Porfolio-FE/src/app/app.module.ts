0e9bb3f8ce0f43f03b13a4e1f96dac794e7386f0
Nicolas <ngacevedo5@gmail.com>
2022-08-10 18:26:32 -0300

    Cambios Y Agregados de imagen
    Cambie datos y agregue unas imagenes extra a mi trabajo

--- Porfolio-FE/src/app/app.module.ts
+++ Porfolio-FE/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/ha
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CertificadosComponent } from './components/certificados/certificados.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -32,7 +33,9 @@ import { CertificadosComponent } from './components/certificados/certificados.co
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
