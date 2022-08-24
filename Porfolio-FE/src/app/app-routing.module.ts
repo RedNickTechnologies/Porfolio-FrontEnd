e8381b4eefe49488eda81f60c5c8f94b5d21b7ff
Nicolas <ngacevedo5@gmail.com>
2022-08-24 19:55:39 -0300

    correccion porcentajes

--- Porfolio-FE/src/app/app-routing.module.ts
+++ Porfolio-FE/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+<<<<<<< Updated upstream
 /* aca se colocan las rutas */
 
+=======
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent}
+  //{path:'/experiencias/crear', component: NewExperienciaComponent}
+>>>>>>> Stashed changes
 ];
 
 @NgModule({
