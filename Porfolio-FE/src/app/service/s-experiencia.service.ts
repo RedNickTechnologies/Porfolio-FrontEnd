a5996f63473942eed4c8278e9e9d7ba5cae1fbcc
Nicolas <ngacevedo5@gmail.com>
2022-08-28 20:45:03 -0300

    Update s-experiencia.service.ts

--- Porfolio-FE/src/app/service/s-experiencia.service.ts
+++ Porfolio-FE/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expURL ='http://localhost:8080/experiencias'
+  expURL ='http://localhost:8080/experiencias/'
 
   constructor(private httpClient: HttpClient) { }
   
