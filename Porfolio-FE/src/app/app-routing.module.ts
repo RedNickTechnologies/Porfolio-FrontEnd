c189467b4a7d8b2c628dadcd7a1285448fb2d725
Nicolas <ngacevedo5@gmail.com>
2022-08-29 14:14:20 -0300

    Correccion de rutas del componente experiencia
    realizando los ultimos cambios para poder editar los distintos componentes dentro de mi portfolio

--- Porfolio-FE/src/app/app-routing.module.ts
+++ Porfolio-FE/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'login', component: LoginComponent}
-  //{path:'/experiencias/crear', component: NewExperienciaComponent}
+  {path:'login', component: LoginComponent},
+  {path:'/nuevaExp', component: NewExperienciaComponent}
 ];
 
 @NgModule({
