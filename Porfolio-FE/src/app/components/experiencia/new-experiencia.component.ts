e2aae84740174519f2263ea87dfb34f450a5758d
Nicolas <ngacevedo5@gmail.com>
2022-10-21 15:01:22 -0300

    correccion errores

--- Porfolio-FE/src/app/components/experiencia/new-experiencia.component.ts
+++ Porfolio-FE/src/app/components/experiencia/new-experiencia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Experiencia } from 'src/app/model/experiencia';
 import { SExperienciaService } from 'src/app/service/s-experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-new-experiencia',
@@ -13,15 +14,18 @@ export class NewExperienciaComponent implements OnInit {
   nombre: string ='';
   puesto: string ='';
   fecha: string ='';
+  token!: string;
 
-  constructor(private sExperiencia: SExperienciaService, private router: Router) { }
+  constructor(private sExperiencia: SExperienciaService, private router: Router, private tokens: TokenService) {
+    this.token=tokens.getToken();
+   }
 
   ngOnInit(): void {
   }
 
   onCreate(): void{
     const exp = new Experiencia(this.nombre, this.puesto, this.fecha);
-    this.sExperiencia.save(exp).subscribe(
+    this.sExperiencia.save(exp,this.token).subscribe(
       data=> {
         alert("Experiencia añadida");
         this.router.navigate(['']);
--- Porfolio-FE/src/app/service/s-experiencia.service.ts
+++ Porfolio-FE/src/app/service/s-experiencia.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Experiencia } from '../model/experiencia';
@@ -19,9 +19,15 @@ export class SExperienciaService {
     return this.httpClient.get<Experiencia>(this.expURL + `detail/${id}`);
   }
 
-  public save(experiencia: Experiencia): Observable<any>{
+  /*public save(experiencia: Experiencia): Observable<any>{
     return this.httpClient.post<any>(this.expURL + `crear`, experiencia );
+  }*/
+
+  public save(experiencia: Experiencia, token: string): Observable<any> {
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
+    return this.httpClient.post<any>(this.expURL + `crear`, experiencia, { headers: headers });
   }
+
 
   public update(id: number, experiencia: Experiencia) : Observable<any>{
     return this.httpClient.put<any>(this.expURL + `editar/${id}`, experiencia);
