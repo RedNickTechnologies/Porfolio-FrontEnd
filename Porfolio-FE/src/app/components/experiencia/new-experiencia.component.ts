c75d37d098373422968a6842d2c23d680c268312
Nicolas <ngacevedo5@gmail.com>
2022-10-21 16:31:57 -0300

    Revert "correccion errores"
    This reverts commit e2aae84740174519f2263ea87dfb34f450a5758d.

--- Porfolio-FE/src/app/components/experiencia/new-experiencia.component.ts
+++ Porfolio-FE/src/app/components/experiencia/new-experiencia.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Experiencia } from 'src/app/model/experiencia';
 import { SExperienciaService } from 'src/app/service/s-experiencia.service';
-import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-new-experiencia',
@@ -14,18 +13,15 @@ export class NewExperienciaComponent implements OnInit {
   nombre: string ='';
   puesto: string ='';
   fecha: string ='';
-  token!: string;
 
-  constructor(private sExperiencia: SExperienciaService, private router: Router, private tokens: TokenService) {
-    this.token=tokens.getToken();
-   }
+  constructor(private sExperiencia: SExperienciaService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onCreate(): void{
     const exp = new Experiencia(this.nombre, this.puesto, this.fecha);
-    this.sExperiencia.save(exp,this.token).subscribe(
+    this.sExperiencia.save(exp).subscribe(
       data=> {
         alert("Experiencia añadida");
         this.router.navigate(['']);
