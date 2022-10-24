be199b48bc637a2ff3ea80d613f242444621cbaa
Nicolas <ngacevedo5@gmail.com>
2022-10-24 15:57:26 -0300

    nuevos componentes agregados
    componentes nuevos agregados, educacion etc

--- Porfolio-FE/src/app/components/experiencia/experiencia.component.ts
+++ Porfolio-FE/src/app/components/experiencia/experiencia.component.ts
@@ -30,6 +30,20 @@ export class ExperienciaComponent implements OnInit {
     this.sExperiencia.lista().subscribe(data => {this.exp = data;})
   }
 
+  delete (id?:number){
+
+    if(id!= undefined){
+      this.sExperiencia.delete(id).subscribe(
+        data =>{
+          this.cargarExperiencia();
+        }, err => {
+          alert("No se pudo borrar la experiencia");
+        }
+      )
+    }
+
+  }
+
 }
 
 
