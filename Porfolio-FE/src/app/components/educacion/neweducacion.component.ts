c3bcd246b7a2404d9044db812010e285426653c1
Nicolas <ngacevedo5@gmail.com>
2022-10-27 13:33:46 -0300

    Corrigiendo las rutas y preparando los demas componentes
    Repare algunos errores de Routing que tenia el front end para poder realizar envios de datos

--- Porfolio-FE/src/app/components/educacion/neweducacion.component.ts
+++ Porfolio-FE/src/app/components/educacion/neweducacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
 
 @Component({
   selector: 'app-neweducacion',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeweducacionComponent implements OnInit {
 
-  constructor() { }
+  nombreE: string;
+  descripcionE: string;
+
+  constructor(private educacionS: EducacionService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const educacion = new Educacion(this.nombreE, this.descripcionE);
+    this.educacionS.save(educacion).subscribe(
+      data =>{
+        alert("Educacion añadida correctamente");
+        this.router.navigate(['']);
+      }, err =>{
+        alert("Falló");
+        this.router.navigate(['']);
+      }
+    )
   }
 
 }
