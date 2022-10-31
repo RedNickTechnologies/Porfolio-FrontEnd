030e6fcae9cbbe71677d19e98a8e99e01083b6be
Nicolas <ngacevedo5@gmail.com>
2022-10-31 11:28:11 -0300

    corrigiendo errores en SERVICE

--- Porfolio-FE/src/app/service/educacion.service.ts
+++ Porfolio-FE/src/app/service/educacion.service.ts
@@ -15,23 +15,23 @@ export class EducacionService {
   constructor(private httpClient : HttpClient) { }
 
   public lista (): Observable<Educacion[]>{
-    return this.httpClient.get<Educacion[]>(this.URL + 'lista');
+    return this.httpClient.get<Educacion[]>(this.URL + 'traer');
   }
 
   public details (id: number): Observable<Educacion>{
-    return this.httpClient.get<Educacion>(this.URL + `detail/${id}`);
+    return this.httpClient.get<Educacion>(this.URL + `${id}`);
   }
 
   public save(educacion : Educacion): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', educacion);
+    return this.httpClient.post<any>(this.URL + 'crear', educacion);
   }
 
   public update(id: number, educacion: Educacion): Observable<any>{
-    return this.httpClient.put<any>(this.URL + `update/${id}`, educacion);
+    return this.httpClient.put<any>(this.URL + `editar/${id}`, educacion);
   }
 
   public delete(id:number): Observable<any>{
-    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+    return this.httpClient.delete<any>(this.URL + `borrar/${id}`);
   }
 
 }
--- Porfolio-FE/src/app/service/skill.service.ts
+++ Porfolio-FE/src/app/service/skill.service.ts
@@ -8,29 +8,28 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = environment.URL + 'skill/';
+  URL = environment.URL + 'skills/';
 
   constructor(private httpClient: HttpClient) {}
 
   public lista(): Observable<Skill[]> {
-    return this.httpClient.get<Skill[]>(this.URL + 'lista');
+    return this.httpClient.get<Skill[]>(this.URL + 'traer');
   }
 
   public detail(id: number): Observable<Skill> {
-    return this.httpClient.get<Skill>(this.URL + 'detail/${id}');
+    return this.httpClient.get<Skill>(this.URL + '${id}');
   }
 
   public save(skill: Skill): Observable<any> {
-    return this.httpClient.post<any>(this.URL + 'create', skill);
+    return this.httpClient.post<any>(this.URL + 'crear', skill);
   }
 
-  public update(id: number, skill: Skill): Observable<any> {
-    return this.httpClient.put<any>(this.URL + `update/${id}`, skill);
+public update(id: number, skill: Skill): Observable<any> {
+    return this.httpClient.put<any>(this.URL + `editar/${id}`, skill);
   }
   public delete(id: number): Observable<any> {
-    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
-  }
-
+    return this.httpClient.delete<any>(this.URL + `borrar/${id}`);
+}
 
 
 }
